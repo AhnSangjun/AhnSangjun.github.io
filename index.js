5c66ab1f625476407a60d5fe09d1b7550e8559bc
AhnSangjun <sdjrdj@naver.com>
2022-05-14 14:42:21 +0900

    회원가입 약관 동으 ㅣ

---
+++ index.js
@@ -0,0 +1,63 @@
+import "./styles.css";
+
+("use strict");
+
+const form = document.querySelector("#form__wrap"); // 데이터를 전송하는 Form
+const checkAll = document.querySelector(".terms__check__all input"); // 모두 동의 체크박스
+const checkBoxes = document.querySelectorAll(".input__check input"); // 모두 동의를 제외한 3개의 체크박스
+const submitButton = document.querySelector("button"); // 확인버튼
+
+const agreements = {
+  termsOfService: false, // 첫번째 필수동의 체크박스
+  privacyPolicy: false, // 두번째 필수동의 체크박스
+  allowPromotions: false, // 이벤트 수신 동의 체크박스
+};
+
+form.addEventListener("submit", (e) => e.preventDefault());
+
+checkBoxes.forEach((item) => item.addEventListener("input", toggleCheckbox));
+
+function toggleCheckbox(e) {
+  const { checked, id } = e.target;
+  agreements[id] = checked;
+  this.parentNode.classList.toggle("active"); // 여기선 중요하지 않음
+  checkAllStatus();
+  toggleSubmitButton();
+} 
+
+function checkAllStatus() {
+  const { termsOfService, privacyPolicy, allowPromotions } = agreements;
+  if (termsOfService && privacyPolicy && allowPromotions) {
+    checkAll.checked = true;
+  } else {
+    checkAll.checked = false;
+  }
+}
+
+
+function toggleSubmitButton() {
+  const { termsOfService, privacyPolicy } = agreements;
+  if (termsOfService && privacyPolicy) {
+    submitButton.disabled = false; // 버튼 활성화
+  } else {
+    submitButton.disabled = true; // 버튼 활성화
+  }
+} 
+
+checkAll.addEventListener("click", (e) => {
+  const { checked } = e.target;
+  if (checked) {
+    checkBoxes.forEach((item) => {
+      item.checked = true;
+      agreements[item.id] = true;
+      item.parentNode.classList.add("active");
+    });
+  } else {
+    checkBoxes.forEach((item) => {
+      item.checked = false;
+      agreements[item.id] = false;
+      item.parentNode.classList.remove("active");
+    });
+  }
+  toggleSubmitButton();
+});
